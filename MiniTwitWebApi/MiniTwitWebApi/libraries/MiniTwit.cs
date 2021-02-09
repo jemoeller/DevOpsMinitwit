05e37130304d982f66c7f139f0b5f0d534307f9e
Jeppe Møller <jeppemmoeller@gmail.com>
2021-02-09 11:08:21 +0100

    add WebAPI project solution
    add monodevelop standard webAPI solution

---
+++ MiniTwitWebApi/MiniTwitWebApi/libraries/MiniTwit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MiniTwitWebApi.libraries
+{
+    public class MiniTwit
+    {
+        private readonly HttpClient _client;
+        private string DATABASE = "/tmp/minitwit.db";
+        private int PER_PAGE = 30;
+        private Boolean DEBUG = true;
+        private string SECRET_KEY = "development key";
+
+        public MiniTwit()
+        {
+
+            var client = new HttpClient();
+
+        }
+
+
+
+    }
+}
