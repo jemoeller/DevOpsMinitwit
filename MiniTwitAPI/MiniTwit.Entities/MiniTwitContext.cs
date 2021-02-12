c3c2342f8f0b1522b51bd532c60529d98614d6f0
jemoeller <Jeppemmoeller@gmail.com>
2021-02-12 15:56:43 +0100

    fix project structure, references, framework targets, repo, context

---
+++ MiniTwitAPI/MiniTwit.Entities/MiniTwitContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MiniTwit.Entities
+{
+    public partial class MiniTwitContext : DbContext, IMiniTwitContext
+    {
+        public virtual DbSet<Follower> Followers { get; set; }
+
+        public virtual DbSet<Message> Messages { get; set; }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public MiniTwitContext(DbContextOptions<MiniTwitContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);//why do we call this method?
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);//do we need this?
+
+    }
+}
---
+++ MiniTwitAPI/MiniTwit.Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace MiniTwit.Entities
+{
+    [Table("user")]
+    public partial class User
+    {
+        [Key]
+        [Column("user_id", TypeName = "integer")]
+        public long UserId { get; set; }
+        [Required]
+        [Column("username", TypeName = "string")]
+        public byte[] Username { get; set; }
+        [Required]
+        [Column("email", TypeName = "string")]
+        public byte[] Email { get; set; }
+        [Required]
+        [Column("pw_hash", TypeName = "string")]
+        public byte[] PwHash { get; set; }
+    }
+}
